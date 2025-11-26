3420bfd4a947650411c723c95bf52320ce5e65a9
Laurence042 <laurence042.mail@gmail.com>
2025-11-26 22:57:48 +0800

    fix: correct base path in vite.config.ts for accuracy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/projects/rimworld-ordinatus-calculi/demo/',
+  base: '/project/rimworld-ordinatus-calculi/demo/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
